2a79315df17bdcc371c56a35ff9fe46eccf30d5f
Gabriel <gabriel.carvalho@beelegal.com>
2023-11-17 11:56:37 -0300

    mudando url do io para qualquer aceitar vercel

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import  Express  from "express";
 import { Server } from "socket.io";
 import { createServer} from 'http'
+import cors from 'cors'
 
 const app = Express()
 const server = createServer(app)
 
-const io = new Server(server , {cors : {origin: '*' },} , )
+const io = new Server(server)
+
+app.use(cors({
+    origin: 'https://front-end-chat-67re-rmmt7lo0a-biel192501ma.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  }));
+  
 
 io.on('connection', socket =>{
     // console.log('user conected', socket.id)
