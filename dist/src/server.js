e132a1a0399aa8b629d8adf9f9c51c037a113a34
Gabriel <gabriel.carvalho@beelegal.com>
2023-11-18 12:05:05 -0300

    add get dist 2 ok

--- dist/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./http/server");
-require("../src/websocket/chat");
-require("../Db");
-const port = process.env.PORT || 3333;
-server_1.server.listen(port, () => {
-    console.log(`server is running in port${port}`);
-});
